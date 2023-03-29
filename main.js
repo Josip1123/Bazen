ced384bd024451ddc624ebac2f465be526f6cb85
Josip1123 <jopetk@hotmail.com>
2023-03-29 14:47:37 +0200

    testing with "before/after" slider

--- main.js
+++ main.js
@@ -4,3 +4,46 @@ const container = document.querySelector('.img-slider_section');
 document.querySelector('.slider').addEventListener('input', (e) => {
     container.style.setProperty('--position', `${e.target.value}%`);
 })
+
+
+// before and after picture slider
+
+let i = 0;
+const beforePictures = ["/Styles/before.png", "/Styles/before2.png"];
+const afterPictures = ["/Styles/after.png", "/Styles/after2.png"];
+
+
+let sliderPictureBefore = document.getElementById("before_image");
+let sliderPictureAfter = document.getElementById("after_image");
+sliderPictureBefore.src = beforePictures[i];
+sliderPictureAfter.src = afterPictures[i];
+
+const leftBtn=document.getElementById("left-slider-btn");
+const rightBtn=document.getElementById("right-slider-btn");
+
+// leftBtn.addEventListener("click", changePicture);
+// rightBtn.addEventListener("click", changePicture);
+
+[leftBtn, rightBtn].forEach(function(btn){
+    btn.addEventListener("click", changePicture)
+})
+
+// function changePicture () {
+
+//     if (i < beforePictures.length - 1 && i < afterPictures.length -1) {
+//         i++
+//     } else {
+//         i=0;
+//     }
+
+//     sliderPictureLeft.src = beforePictures[i];
+//     sliderPictureRight.src = afterPictures[i];
+// }
+
+function changePicture () {
+
+    i++;
+    sliderPictureBefore.src = beforePictures[i % beforePictures.length];
+    sliderPictureAfter.src = afterPictures[i % afterPictures.length];
+    
+}
