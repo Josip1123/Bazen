946937a14c84f1bc1a8c941a29c0e9ecb21e02d6
Josip1123 <jopetk@hotmail.com>
2023-04-16 22:00:12 +0200

    changes to imgs, css and JS

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// image slider
+// img slider
 
 const container = document.querySelector(".img-slider_section");
 
@@ -6,7 +6,7 @@ document.querySelector(".slider").addEventListener("input", (e) => {
     container.style.setProperty("--position", `${e.target.value}%`);
 });
 
-// before and after picture slider
+// before and after pictures changer
 
 const beforePictures = ["./Styles/img/before.png", "./Styles/img/before2.png"];
 const afterPictures = ["./Styles/img/after.png", "./Styles/img/after2.png"];
@@ -14,15 +14,9 @@ const afterPictures = ["./Styles/img/after.png", "./Styles/img/after2.png"];
 let sliderPictureBefore = document.getElementById("before_image");
 let sliderPictureAfter = document.getElementById("after_image");
 let i = 0;
-sliderPictureBefore.src = beforePictures[i];
-sliderPictureAfter.src = afterPictures[i];
 
 const leftBtn = document.getElementById("left-slider-btn");
 const rightBtn = document.getElementById("right-slider-btn");
-
-// [leftBtn, rightBtn].forEach(function (btn) {
-//     btn.addEventListener("click", changePicture);
-// });
 
 [leftBtn, rightBtn].forEach((btn) => {
     btn.addEventListener("click", changePicture);
